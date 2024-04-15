c8700c69080bbc977faa52fb05330250b6ad1a47
AJ Fragoso <ajf1118@rit.edu>
2024-04-14 21:26:46 -0400

    Initial Commit

---
+++ ConnectionsSwiftUI/ConnectionsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectionsSwiftUIApp.swift
+//  ConnectionsSwiftUI
+//
+//  Created by AJ Fragoso on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ConnectionsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
